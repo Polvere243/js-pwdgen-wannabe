3304fa994960ac7ce644d9d7a5a33eeb9709f8a2
Polvere243 <danielercoli@gmail.com>
2023-12-15 19:23:57 +0100

    did and checked both the seventh and the eighth steps

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.getElementById(password);
 
 // punto 2: chiedere il nome all'utente
 
-prompt("Come ti chiami", "Daniele");
+prompt("Come ti chiami?", "Daniele");
 
 
 
@@ -16,7 +16,7 @@ console.log(firstName);
 
 // punto 4: chiedere il cognome all'utente
 
-prompt("Qual è il tuo cognome", "Ercoli");
+prompt("Qual è il tuo cognome?", "Ercoli");
 
 // punto 5: ripetere il punto 3
 
@@ -27,3 +27,15 @@ console.log(lastName);
 // punto 6: chiedere il colore preferito all'utente
 
 prompt("Qual è il tuo colore preferito?", "rosso");
+
+// punto 7: ripeti il punto 3
+
+const favouriteColour = "Rosso";
+
+console.log(favouriteColour);
+
+// punto 8: creare un messaggio per introdurre la password
+
+let passwordMessage = "Ecco la tua nuova password ";
+
+console.log(passwordMessage);
