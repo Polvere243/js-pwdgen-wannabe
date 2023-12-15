8703dada7682792dfd24d1e490283fe004d52b06
Polvere243 <danielercoli@gmail.com>
2023-12-15 18:50:00 +0100

    did and checked the second step

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
+// punto 1: recupero dell'elemento
+
 document.getElementById(password);
-console.log(password)
+
+// punto 2: chiedere il nome all'utente
+
+prompt("Come ti chiami", "Daniele");
+
+console.log("prompt");
